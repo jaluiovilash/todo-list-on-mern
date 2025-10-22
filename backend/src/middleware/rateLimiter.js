50d1295d0fe55310235f5f52775a54f1aee91637
jaluiovilash <ovilashjalui@gmail.com>
2025-10-23 00:38:51 +0530

    Adding backend and README file... will push frontend later

---
+++ backend/src/middleware/rateLimiter.js
@@ -0,0 +1,20 @@
+import ratelimit from "../config/upstash.js";
+
+const rateLimiter = async (_, res, next) => {
+  try {
+    const { success } = await ratelimit.limit("my-rate-limit");
+
+    if (!success) {
+      return res.status(429).json({
+        message: "Too many requests, please try after 5 minutes",
+      });
+    }
+
+    next();
+  } catch (error) {
+    console.log("Rate limit error", error);
+    next(error);
+  }
+};
+
+export default rateLimiter;
---
+++ backend/src/server.js
@@ -0,0 +1,50 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import path from "path";
+
+import notesRoutes from "./routes/notesRoutes.js";
+import { connectDB } from "./config/db.js";
+import rateLimiter from "./middleware/rateLimiter.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
+
+// Middleware
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173", // frontend dev server
+    })
+  );
+}
+
+app.use(express.json()); // Parse JSON request body
+app.use(rateLimiter);
+
+// Routes
+app.use("/api/notes", notesRoutes);
+
+// Serve frontend in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
+// Connect DB, then start server
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server started at http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Failed to connect to MongoDB:", err.message);
+    process.exit(1);
+  });
